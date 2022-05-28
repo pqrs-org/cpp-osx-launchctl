68d1d16bb65712622c8b476bb3b5a1a8ecc97755
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 08:00:44 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,2 +1,32 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <boost/ut.hpp>
+#include <pqrs/osx/launchctl.hpp>
+
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+
+  "domain_target"_test = [] {
+    expect(pqrs::osx::launchctl::make_system_domain_target() ==
+           pqrs::osx::launchctl::domain_target("system/"));
+
+    expect(pqrs::osx::launchctl::make_gui_domain_target(201) ==
+           pqrs::osx::launchctl::domain_target("gui/201/"));
+
+    expect(pqrs::osx::launchctl::make_gui_domain_target(501) ==
+           pqrs::osx::launchctl::domain_target("gui/501/"));
+  };
+
+  "service_target"_test = [] {
+    auto system_domain_target = pqrs::osx::launchctl::make_system_domain_target();
+    auto gui_domain_target = pqrs::osx::launchctl::make_gui_domain_target(501);
+    pqrs::osx::launchctl::service_name service_name("org.pqrs.example");
+
+    expect(pqrs::osx::launchctl::make_service_target(system_domain_target, service_name) ==
+           pqrs::osx::launchctl::service_target("system/org.pqrs.example"));
+
+    expect(pqrs::osx::launchctl::make_service_target(gui_domain_target, service_name) ==
+           pqrs::osx::launchctl::service_target("gui/501/org.pqrs.example"));
+  };
+
+  return 0;
+}
